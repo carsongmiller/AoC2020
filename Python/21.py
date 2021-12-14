93d9dd2681f667f44f1a2d0617ac45b713333de5
carsongmiller <carsongmiller@gmail.com>
2021-12-14 16:10:13 -0600

    Added some python

---
+++ Python/21.py
@@ -0,0 +1,28 @@
+from timeit import default_timer as timer
+
+lines = str()
+with open('21_input.txt') as f:
+	lines = [n.strip() for n in f.readlines()]
+
+def part1(lines):
+	pass
+
+def part2(lines):
+	pass
+
+
+
+
+start = timer()
+p1 = part1(lines)
+end = timer()
+print("Part 1:", p1)
+print("Time (msec):", (end - start) * 1000)
+print()
+
+start = timer()
+p2 = part2(lines)
+end = timer()
+print("Part 2:", p2)
+print("Time (msec):", (end - start) * 1000)
+print()
